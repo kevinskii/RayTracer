4b4e88c44b0adbe22060a2a22bc33764746b27f0
Kevin Davis <kevinskii.ps@gmail.com>
2019-01-24 23:30:42 -0800

    Update RayTracer.cpp
    Added diffusion

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -7,16 +7,27 @@
 #include "camera.h"
 #include "float.h"
 
-
+// Mimics the drand48() library function in the book that generates a random number in [0,1.0).
 double drand48() {
 	// Ref: http://forums.codeguru.com/showthread.php?525211-How-to-use-drand48()-for-Windows
 	return rand() / (RAND_MAX + 1.0);
 }
 
+// Finds a random point in a sphere with unit radius centered at the origin
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0f * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0f);
+	return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0f, FLT_MAX, rec)) {
-		return 0.5f * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+	// use 0.001f rather than 0.0f for t_min to get rid of the "shadow acne" problem (Ch 7).
+	if (world->hit(r, 0.001f, FLT_MAX, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5f * color(ray(rec.p, target - rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -37,8 +48,14 @@ int main()
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
 	camera cam;
+	// From bottom to top
 	for (int j = ny - 1; j >= 0; j--) {
+		// From left to right
 		for (int i = 0; i < nx; i++) {
+
+			// Choose the color of this ray based on random samples of several close by locations
+			// to as to anti-alias, i.e. to blur the edges of objects so they look smoother and less
+			// jagged.
 			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++) {
 				float u = float(i + drand48()) / float(nx);
@@ -47,6 +64,11 @@ int main()
 				col += color(r, world);
 			}
 			col /= float(ns);
+
+			// Most image viewers assume the image is gamma corrected, so they make it darker.
+			// Use "gamma 2", i.e. raise the color to the power 1/gamma=1/2, i.e. take the square root.
+			col = vec3(sqrt(col[0]), (col[1]), (col[2]));
+
 			int ir = int(255.99f*col[0]);
 			int ig = int(255.99f*col[1]);
 			int ib = int(255.99f*col[2]);
